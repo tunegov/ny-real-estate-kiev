2785ee759fdb0fd5229c232f25e4e237b624009f
Sergey Tunegov <sergeytunegov@Sergeys-MacBook-Pro.local>
2020-03-03 22:33:09 +0200

    upd

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const SitemapGenerator = require('advanced-sitemap-generator');
 const path = require('path');
 // create generator
-const generator = SitemapGenerator('http://localhost:3000', {
+const generator = SitemapGenerator('https://ny.com.ua', {
   ignoreHreflang: true,
   maxDepth: 0,
   filepath: path.join(process.cwd(), 'sitemap.xml'),
